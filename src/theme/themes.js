281530977b9b73005f74792f93dadd8a3b074e7b
Peled12 <korenpeled08@gmail.com>
2025-03-01 19:49:40 +0200

    settings change

--- src/theme/themes.js
+++ src/theme/themes.js
@@ -2,15 +2,6 @@ export const lightTheme = {
   background: '#ebe5db',
   color: '#121212',
   accentColor: '#3D84C6',
-  chronoTheme: {
-    cardBgColor: 'white',
-    cardForeColor: 'black',
-    titleColor: 'white',
-  },
-  timelineLineColor: '#ccc',
-  cardBackground: '#ece9e4',
-  cardFooterBackground: '#f7f7f7',
-  cardBorderColor: '#00000020',
   navbarTheme: {
     linkColor: '#dedede',
     linkHoverColor: '#fefefe',
@@ -25,15 +16,6 @@ export const darkTheme = {
   background: '#010101',
   color: '#eee',
   accentColor: '#3D84C6',
-  chronoTheme: {
-    cardBgColor: '#1B1B1B',
-    cardForeColor: '#eee',
-    titleColor: 'black',
-  },
-  timelineLineColor: '#444',
-  cardBackground: '#060606',
-  cardFooterBackground: '#181818',
-  cardBorderColor: '#ffffff20',
   navbarTheme: {
     linkColor: '#dedede',
     linkHoverColor: '#fefefe',
